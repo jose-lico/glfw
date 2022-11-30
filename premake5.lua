2d3cc8f15216caa4ba4f204628b8fa1f0c0d34e1
José Lico <57019940+jose-lico@users.noreply.github.com>
2022-11-30 19:15:02 +0000

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,11 @@
 project "glfw3"
 	kind "StaticLib"
 	language "C"
-	configmap {
-        ["ReleaseNoImGui"] = "Release"
-    }
+	configmap 
+	{
+		["ReleaseWithInfo"] = "Release"
+		["ReleaseNoImGui"] = "Release"
+	}
 
 	targetdir ("%{wks.location}/bin/" .. outputDir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. outputDir .. "/%{prj.name}")
@@ -45,7 +47,6 @@ project "glfw3"
 		}
 
 	filter "system:linux"
-		pic "On"
 		systemversion "latest"
 
 		files
